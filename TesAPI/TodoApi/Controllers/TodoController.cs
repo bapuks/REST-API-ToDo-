14c2e71424572fd9577636917ca1fb337739a729
bapuks <benni.bangun@gmail.com>
2020-04-11 22:44:24 +0700

    This is code for Live Coding Test for .NET Developer

---
+++ TesAPI/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    //Defines an API controller class without methods
+    //Marks the class with the [ApiController] attribute.
+    //Uses DI to inject the database context (TodoContext) into the controller. 
+    //Adds an item named Item1 to the database if the database is empty. This code is in the constructor, so it runs every time there's a new HTTP request. If you delete all items, the constructor creates Item1 again the next time an API method is called.
+
+    [Route("api/todo")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { PercentComplete = "0" });
+                _context.TodoItems.Add(new TodoItem { Title = "Title1" });
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        //To provide an API that retrieves to-do items
+        //These methods implement two GET endpoints
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        //In the following GetTodoItem method, "{id}" is a placeholder variable for the unique identifier of the to-do item. When GetTodoItem is invoked, the value of "{id}" in the URL is provided to the method in itsid parameter
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // This code is an HTTP POST method
+        // The method gets the value of the to-do item from the body of the HTTP request
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            // This method returns an HTTP 201 status code, if successful and add a Location header to the response
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+
+        // PutTodoItem is similar to PostTodoItem, except it uses HTTP PUT
+        // A PUT request requires the client to send the entire updated entity, not just the changes
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DeleteTodoItem method to delete a to-do item
+        // This method allows you to delete all the items. However, when the last item is deleted, a new one is created by the model class constructor the next time the API is called
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ TesAPI/TodoApi/wwwroot/site.js
@@ -0,0 +1,151 @@
+﻿const uri = "api/todo";
+let todos = null;
+function getCount(data) {
+    const el = $("#counter");
+    let name = "to-do";
+    if (data) {
+        if (data > 1) {
+            name = "to-dos";
+        }
+        el.text(data + " " + name);
+    } else {
+        el.text("No " + name);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+});
+
+// jQuery sends an HTTP GET request to the web API, which returns JSON representing an array of to-do items.
+// The success callback function is invoked if the request succeeds.
+function getData() {    
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#todos");
+
+            $(tBody).empty();
+
+            getCount(data.length);
+
+            $.each(data, function (key, item) {
+                const tr = $("<tr></tr>")
+                    .append(
+                        $("<td></td>").append(
+                            $("<input/>", {
+                                type: "checkbox",
+                                disabled: true,
+                                checked: item.isComplete
+                            })
+                        )
+                )
+                    .append($("<td></td>").text(item.percentComplete))
+                    .append($("<td></td>").text(item.title))
+                    .append($("<td></td>").text(item.name))
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Edit</button>").on("click", function () {
+                                editItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Delete</button>").on("click", function () {
+                                deleteItem(item.id);
+                            })
+                        )
+                    )                    
+                    ;
+
+                tr.appendTo(tBody);
+            });
+
+            todos = data;
+        }
+    });
+}
+
+
+// jQuery sends an HTTP POST request with the to -do item in the request body.The accepts and contentType options are set to application / json to specify the media type being received and sent.
+// The to-do item is converted to JSON by using JSON.stringify.
+// When the API returns a successful status code, the getData function is invoked to update the HTML table.
+function addItem() {
+
+    const item = {
+        name: $("#add-name").val(),
+        isComplete: false
+    };
+
+    $.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri,
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (result) {
+            getData();
+            $("#add-name").val("");
+        }
+    });
+}
+
+// Deleting a to-do item is accomplished by setting the type on the AJAX call to DELETE and specifying the item's unique identifier in the URL.
+function deleteItem(id) {
+    $.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(todos, function (key, item) {
+        if (item.id === id) {            
+            $("#edit-id").val(item.id);
+            $("#edit-isComplete")[0].checked = item.isComplete;   
+            $("#edit-percent-complete").val(item.percentComplete);   
+            $("#edit-title").val(item.title);
+            $("#edit-name").val(item.name);
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function () {
+    const item = {
+        percentComplete: $("#edit-percent-complete").val(),
+        title: $("#edit-title").val(),
+        name: $("#edit-name").val(),
+        isComplete: $("#edit-isComplete").is(":checked"),        
+        id: $("#edit-id").val()
+    };
+
+    //Updating a to-do item is similar to adding one.
+    //The url changes to add the unique identifier of the item, and the type is PUT
+    $.ajax({
+        url: uri + "/" + $("#edit-id").val(),
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    $("#spoiler").css({ display: "none" });
+}
